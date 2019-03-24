2fbc01fa7c675137b7596ffbe354d901bedafa6d
Mark W <markwuu@gmail.com>
2019-03-23 16:28:45 -1000

    Animated SVG icons

--- src/utils/typography.js
+++ src/utils/typography.js
@@ -1,7 +1,8 @@
 import Typography from "typography"
 import sternGroveTheme from "typography-theme-stern-grove"
+import altonTheme from "typography-theme-alton"
 
-const typography = new Typography(sternGroveTheme)
+const typography = new Typography(altonTheme)
 
 export const { scale, rhythm, options } = typography
 export default typography
