5e1a09b37a8d8c4c1bb045ceee2ef83f66855f12
Mark W <markwuu@gmail.com>
2019-05-09 18:43:59 -0700

    Refactored icons from styled components into css modules

--- assets/icons.js
+++ assets/icons.js
@@ -1,69 +1,46 @@
 import React from "react"
-import styled from "styled-components"
+import iconStyles from "./icons.module.css"
+// import styled from "styled-components"
 
-const SVG = styled.svg`
-  fill: white;
-  transition: fill 0.35s;
-  width: 30px;
-  height: 30px;
-  margin-left: 10px;
-  margin-right: 10px;
+// const SVG = styled.svg`
+//   fill: white;
+//   transition: fill 0.35s;
+//   width: 30px;
+//   height: 30px;
+//   margin-left: 10px;
+//   margin-right: 10px;
 
-  &:hover {
-    fill: white;
-  }
-`
+//   &:hover {
+//     fill: white;
+//   }
+// `
 
 export const LinkedinIcon = () => (
   <a href="https://www.linkedin.com/in/markwuu/">
-    <SVG viewBox="0 0 24 24">
+    <svg className={iconStyles.icon} viewBox="0 0 24 24">
       <path d="M20.447 20.452h-3.554v-5.569c0-1.328-.027-3.037-1.852-3.037-1.853 0-2.136 1.445-2.136 2.939v5.667H9.351V9h3.414v1.561h.046c.477-.9 1.637-1.85 3.37-1.85 3.601 0 4.267 2.37 4.267 5.455v6.286zM5.337 7.433c-1.144 0-2.063-.926-2.063-2.065 0-1.138.92-2.063 2.063-2.063 1.14 0 2.064.925 2.064 2.063 0 1.139-.925 2.065-2.064 2.065zm1.782 13.019H3.555V9h3.564v11.452zM22.225 0H1.771C.792 0 0 .774 0 1.729v20.542C0 23.227.792 24 1.771 24h20.451C23.2 24 24 23.227 24 22.271V1.729C24 .774 23.2 0 22.222 0h.003z" />
-    </SVG>
+    </svg>
   </a>
 )
 
 export const GithubIcon = () => (
   <a href="https://github.com/markwuu">
-    <SVG viewBox="0 0 16 16">
+    <svg className={iconStyles.icon} viewBox="0 0 16 16">
       <path
         fillRule="evenodd"
         d="M14.7 5.34c.13-.32.55-1.59-.13-3.31 0 0-1.05-.33-3.44 1.3-1-.28-2.07-.32-3.13-.32s-2.13.04-3.13.32c-2.39-1.64-3.44-1.3-3.44-1.3-.68 1.72-.26 2.99-.13 3.31C.49 6.21 0 7.33 0 8.69 0 13.84 3.33 15 7.98 15S16 13.84 16 8.69c0-1.36-.49-2.48-1.3-3.35zM8 14.02c-3.3 0-5.98-.15-5.98-3.35 0-.76.38-1.48 1.02-2.07 1.07-.98 2.9-.46 4.96-.46 2.07 0 3.88-.52 4.96.46.65.59 1.02 1.3 1.02 2.07 0 3.19-2.68 3.35-5.98 3.35zM5.49 9.01c-.66 0-1.2.8-1.2 1.78s.54 1.79 1.2 1.79c.66 0 1.2-.8 1.2-1.79s-.54-1.78-1.2-1.78zm5.02 0c-.66 0-1.2.79-1.2 1.78s.54 1.79 1.2 1.79c.66 0 1.2-.8 1.2-1.79s-.53-1.78-1.2-1.78z"
       />
-    </SVG>
+    </svg>
   </a>
 )
 
 export const EmailIcon = () => (
   <a href="mailto:markwuu@gmail.com">
-    <SVG viewBox="0 0 14 16">
+    <svg className={iconStyles.icon} viewBox="0 0 14 16">
       <path
         fillRule="evenodd"
         d="M0 4v8c0 .55.45 1 1 1h12c.55 0 1-.45 1-1V4c0-.55-.45-1-1-1H1c-.55 0-1 .45-1 1zm13 0L7 9 1 4h12zM1 5.5l4 3-4 3v-6zM2 12l3.5-3L7 10.5 8.5 9l3.5 3H2zm11-.5l-4-3 4-3v6z"
       />
-    </SVG>
+    </svg>
   </a>
-)
-
-export const KeyboardIcon = () => (
-  <SVG viewBox="0 0 16 16">
-    <path d="M10 5H9V4h1v1zM3 6H2v1h1V6zm5-2H7v1h1V4zM4 4H2v1h2V4zm8 7h2v-1h-2v1zM8 7h1V6H8v1zm-4 3H2v1h2v-1zm8-6h-1v1h1V4zm2 0h-1v1h1V4zm-2 5h2V6h-2v3zm4-6v9c0 .55-.45 1-1 1H1c-.55 0-1-.45-1-1V3c0-.55.45-1 1-1h14c.55 0 1 .45 1 1zm-1 0H1v9h14V3zM6 7h1V6H6v1zm0-3H5v1h1V4zM4 7h1V6H4v1zm1 4h6v-1H5v1zm5-4h1V6h-1v1zM3 8H2v1h1V8zm5 0v1h1V8H8zM6 8v1h1V8H6zM5 8H4v1h1V8zm5 1h1V8h-1v1z" />
-  </SVG>
-)
-
-export const ApplicationIcon = () => (
-  <SVG viewBox="0 0 14 16">
-    <path
-      fillRule="evenodd"
-      d="M5 3h1v1H5V3zM3 3h1v1H3V3zM1 3h1v1H1V3zm12 10H1V5h12v8zm0-9H7V3h6v1zm1-1c0-.55-.45-1-1-1H1c-.55 0-1 .45-1 1v10c0 .55.45 1 1 1h12c.55 0 1-.45 1-1V3z"
-    />
-  </SVG>
-)
-
-export const ContactIcon = () => (
-  <SVG viewBox="0 0 16 16">
-    <path
-      fillRule="evenodd"
-      d="M15 1H6c-.55 0-1 .45-1 1v2H1c-.55 0-1 .45-1 1v6c0 .55.45 1 1 1h1v3l3-3h4c.55 0 1-.45 1-1V9h1l3 3V9h1c.55 0 1-.45 1-1V2c0-.55-.45-1-1-1zM9 11H4.5L3 12.5V11H1V5h4v3c0 .55.45 1 1 1h3v2zm6-3h-2v1.5L11.5 8H6V2h9v6z"
-    />
-  </SVG>
 )
